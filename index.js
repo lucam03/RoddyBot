ed1a0ef3b09042703eb4825baace15db42493497
lucam03 <62672581+lucam03@users.noreply.github.com>
2020-05-27 23:31:50 +0100

    added some more file extensions for images

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const prefix = config.prefix;
 
 var currentPredictions = {};
 var model;
+const acceptedFormats = ["jpg", "jpeg", "png", "webp"];
 
 const loadModel = async () => {
   console.log("Loading model...")
@@ -19,8 +20,8 @@ const loadModel = async () => {
 }
 
 const predictCats = async (imgUrl, message) => {
-  let extension = imgUrl.slice(-4).toLowerCase();
-  if (extension == ".png" || extension == ".jpg") {
+  let extension = imgUrl.split('.').pop().toLowerCase();
+  if (acceptedFormats.includes(extension)) {
     let imgResponse = await axios({url: imgUrl, responseType: "arraybuffer"});
     let imgBuffer = Buffer.from(imgResponse.data, "binary");
     let img = sharp(imgBuffer)
@@ -32,6 +33,7 @@ const predictCats = async (imgUrl, message) => {
         console.log("Guess(es) for "+imgUrl+":");
         console.log(prediction);
         allPredictions = _.uniq(_.pluck(prediction, "class"));
+        allScores = _.uniq(_.pluck(prediction, "score"));
         currentPredictions[message.channel] = prediction.length ? allPredictions : "unsure";
         if (allPredictions.includes("cat")) {
           message.channel.send("roddy");
@@ -58,13 +60,14 @@ client.on("message", message => {
   } else if (message.content.length) {
     let text = message.content.split(" ");
     for (let i=0; i<text.length; i++) {
-      let extension = text[i].slice(-4).toLowerCase();
-      if (extension == ".png" || extension == ".jpg") {
+      let parts = text[i].split(".");
+      let extension = parts.pop().toLowerCase();
+      if (acceptedFormats.includes(extension)) {
         predictCats(text[i], message);
       }
     }
   }
-  if (message.content === `${prefix}guess`) {
+  if (message.content.toLowerCase().startsWith(`${prefix}guess`)) {
     console.log("Guessing...")
     message.channel.send(currentPredictions[message.channel] ? currentPredictions[message.channel] : "i havent seen any pictures yet");
   }
